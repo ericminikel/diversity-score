6d98663313e09b5606fdcfed739b08a7b6f5c315
Eric Minikel <ericminikel@gmail.com>
2014-08-14 15:04:58 -0400

    finished euclid stuff, adding vcf reading

---
+++ diversity_score.py
@@ -0,0 +1,72 @@
+from itertools import combinations
+from scipy.misc import comb
+
+def read_pcs(path,n=9):
+    '''
+    Read principal components from a CSV file at the specified path.
+    First column is sample id, next n are principal components. Additional
+    columns may be present but will be ignored.
+    '''
+    pcs = {}
+    with open(path) as inf:
+        header = inf.readline().strip().split(',')
+        for line in inf.readlines():
+            cols = line.strip().split(',')
+            sampleid = cols[0]
+            samplepcs = [float(col) for col in cols[1:(n+1)]]
+            pcs[sampleid] = samplepcs
+    return pcs
+
+def euclid_dist(coords1, coords2):
+    '''
+    Given two equal-length lists of coordinates in multi-dimensional space,
+    return the Euclidean distance between the two points.
+    '''
+    assert len(coords1) == len(coords2), "Coordinate vectors differ in length"
+    squared_diffs = [(coords1[i] - coords2[i])**2 for i in range(0,len(coords1))]
+    euclidean_distance = sum(squared_diffs)**.5
+    return (euclidean_distance)
+
+def mean_euclid_dist(samples,pcs):
+    '''
+    Given a list of samples and a dictionary of principal components, calculate
+    the mean Euclidean distance between pairs of samples. For n samples this
+    requires n choose 2 comparisons.
+    '''
+    n = len(samples)
+    assert n <= 100, "Can't do more than 100 samples"
+    n_pairs = comb(n,2,exact=True)
+    mean_euclid_dist = 0.0
+    for pair in combinations(samples,2):
+        # average as you go
+        mean_euclid_dist += euclid_dist(pcs[pair[0]],pcs[pair[1]]) / n_pairs
+    return mean_euclid_dist
+
+def get_vcf_colnames(vcfpath):
+    '''
+    Open a (bgzipped) VCF file and return a list of column names from the 
+    #CHROM line. Adapted from Konrad Karczewski.
+    '''
+    with gzip.open(refvcf) as f:
+        for line in f:
+            if line.startswith('#CHROM'):
+                column_names = line.strip().split("\t") # need "\t" otherwise split() breaks when sample ids contain spaces
+                break
+    return column_names
+
+def get_vcf_lines(vcfpath,chr,startpos,endpos):
+    startpos = int(pos) - int(pos_buffer)
+    endpos = int(pos) + int(pos_buffer)
+    tabixfile = pysam.Tabixfile(refvcf)
+    vcfline_generator = tabixfile.fetch(chr,startpos,endpos)
+    lines = list(vcfline_generator)
+    return lines
+
+def get_samples_with_allele(vcfpath,chr,pos,ref,alt):
+    '''
+    Accepts a path to a (bgzipped, tabix-indexed) VCF file, and chr,pos,ref,alt
+    for one allele. Looks at the VCF and returns a list of samples
+    (identified by column headers from the #CHROM line) that have this alt
+    allele.
+    '''
+
