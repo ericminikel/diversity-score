5da5bbc3169df60f6eebc29c8a5ab7fe9a7dd8a8
Eric Minikel <ericminikel@gmail.com>
2014-08-14 15:46:15 -0400

    was parsing VCF but now going to try switch to PyVCF

--- diversity_score.py
+++ diversity_score.py
@@ -1,3 +1,5 @@
+import pysam
+import gzip
 from itertools import combinations
 from scipy.misc import comb
 
@@ -47,20 +49,38 @@ def get_vcf_colnames(vcfpath):
     Open a (bgzipped) VCF file and return a list of column names from the 
     #CHROM line. Adapted from Konrad Karczewski.
     '''
-    with gzip.open(refvcf) as f:
+    with gzip.open(vcfpath) as f:
         for line in f:
             if line.startswith('#CHROM'):
                 column_names = line.strip().split("\t") # need "\t" otherwise split() breaks when sample ids contain spaces
                 break
+    assert column_names[0] == "#CHROM", "First column is not named #CHROM"
+    column_names[0] = "CHROM" # remove the hashtag
     return column_names
 
-def get_vcf_lines(vcfpath,chr,startpos,endpos):
-    startpos = int(pos) - int(pos_buffer)
-    endpos = int(pos) + int(pos_buffer)
-    tabixfile = pysam.Tabixfile(refvcf)
-    vcfline_generator = tabixfile.fetch(chr,startpos,endpos)
+def get_vcf_line(vcfpath,chr,pos):
+    '''
+    Tabix a single line from a VCF file.
+    '''
+    tabixfile = pysam.Tabixfile(vcfpath)
+    vcfline_generator = tabixfile.fetch(chr,pos-1,pos)
     lines = list(vcfline_generator)
-    return lines
+    assert len(lines) <= 1, "Tabix returned >1 line of VCF content for a single position."
+    assert len(lines) == 1, "Tabix found no such line in specified VCF."
+    return lines[0]
+
+def get_vcf_header(vcfpath):
+    '''
+    Accepts a path to a (gzipped) VCF and returns the whole header, as a string.
+    '''
+    header = ''
+    with gzip.open(vcfpath) as f:
+        for line in f:
+            if line.startswith('#'):
+                header += line
+            else:
+                break
+    return header
 
 def get_samples_with_allele(vcfpath,chr,pos,ref,alt):
     '''
@@ -69,4 +89,20 @@ def get_samples_with_allele(vcfpath,chr,pos,ref,alt):
     (identified by column headers from the #CHROM line) that have this alt
     allele.
     '''
+    vcf_colnames = get_vcf_colnames(vcfpath)
+    vcf_line_string = get_vcf_line(vcfpath,chr,pos)
+    vcf_line_cols = vcf_line_string.strip().split('\t')
+    assert len(vcf_colnames) == len(vcf_line_cols), "Column names has length %s but there are %s fields in this line"%(len(vcf_colnames),len(vcf_line_cols))
+    vcf_line = dict((vcf_colnames[i], vcf_line_cols[i]) for i in range(0,len(vcf_colnames)))
+    assert chr == vcf_line['CHROM'], "Extracted contig name does not match input"
+    assert pos == int(vcf_line['POS']), "Extracted position does not match input"
+    alt_alleles = vcf_line['ALT'].split(',')
+    assert alt in alt_alleles, "Specified alt allele does not exist at this site"
+    this_alt_allele_index = alt_alleles.index(alt) # index of this particular allele in comma-separated INFO fields
+    this_alt_allele_number = alt_alleles.index(alt) + 1 # for GT fields, need allele number: 1, 2, etc. remember REF allele is 0.
+    # check that 0 < AC < 100 for this allele.
+    info_fields = vcf_line['INFO'].split(';')
+    info_keys = [info_field.split("=")[0] for info_field in info_fields]
+    info_values = [info_field.split("=")[1] for info_field in info_fields]
+    ac = info_fields[0]
 
