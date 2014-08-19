4f0e55282b4d8b21a6d72b5c14850c9a92e877a6
Eric Minikel <ericminikel@gmail.com>
2014-08-19 13:01:49 -0400

    lousy hack to force PyVCF to use fsock and not filename

--- diversity_score.py
+++ diversity_score.py
@@ -200,8 +200,18 @@ def score_entire_file(pcpath,vcfpath,weightpath,minac=1,maxac=2500,flag='',n_pcs
         openfunc = gzip.open
     else:
         openfunc = open
-    # in theory PyVCF can accept just a path, but I found it only works with an fsock, hence the need for openfunc
-    vcf_reader = vcf.Reader(openfunc(vcfpath),strict_whitespace=True) # split only on tab, allow spaces in ids
+    # in theory PyVCF can accept just a path, but I found it only works with an fsock, hence the need for openfunc (above).
+    # filename='ignore',compressed=False is a lousy hack to force vcf.Reader.__init__ to use the fsock and not the filename
+    # __init__ func currently contains 2 lines:
+    # if filename is None and hasattr(fsock, 'name'):
+    #            filename = fsock.name
+    # which short-circuit my attempt to pass it an fsock; it then opens the file with open(filename,mode='rt') instead of
+    # with gzip, and thus it crashes with this error:
+    # if mode[0:1] == 'r':
+    #     TypeError: 'int' object is not subscriptable
+    # by making the filename non-None, I prevent this; but if PyVCF is later refactored to use filename before fsock,
+    # my code will break. hence I say it's a lousy hack.
+    vcf_reader = vcf.Reader(openfunc(vcfpath),filename='ignore',compressed=False,strict_whitespace=True) # split only on tab, allow spaces in ids
     for record in vcf_reader: # iterate over every row of VCF
         for alt in record.ALT: # for every alt allele at this site
             this_alt_allele_index = record.ALT.index(alt) # index of this particular allele in comma-separated INFO fields
