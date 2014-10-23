583121b89f1f92bf613b8fd6c67f311e04be0a71
Eric Minikel <ericminikel@gmail.com>
2014-10-23 15:38:06 -0400

    computing diversity score is now optional

--- plot_variant_in_pc_space.py
+++ plot_variant_in_pc_space.py
@@ -10,11 +10,12 @@ parser.add_argument('--pcs', '-p', dest='pcs', action='store',
                     help='Principal components (path)', type=str)
 parser.add_argument('--weights', '-w', dest='wts', action='store',
                     help='Weights (path)', type=str)
-
 parser.add_argument('--vcf',  dest='vcf', action='store',
                     help='Genotypes VCF (path)', type=str)
 parser.add_argument('--variant', dest='variant', action='store',
                     help='Whitespace-separated CHROM POS REF ALT', type=str)
+parser.add_argument('--diversity', '-d', dest='diversity', action='store_true',
+                    help='Compute and display diversity score [default false]')
 args = parser.parse_args()
 
 pcs = read_pcs(args.pcs)
@@ -23,6 +24,10 @@ chr, pos, ref, alt = args.variant.split()
 pos = int(pos)
 vcf_header = get_vcf_header(args.vcf)
 samples = get_samples_with_allele(args.vcf,vcf_header,chr,pos,ref,alt)
-meandist = mean_euclid_dist(samples,pcs,weights=wts,warn=False)
+if args.diversity:
+    meandist = mean_euclid_dist(samples,pcs,weights=wts,warn=False)
+    subtitle = 'Diversity score: ' + str(meandist)
+else:
+    subtitle = ''
 
-make_r_plot(chr,pos,ref,alt,meandist,samples,args.pcs)
+make_r_plot(chr,pos,ref,alt,subtitle,samples,args.pcs)
