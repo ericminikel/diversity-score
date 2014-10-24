643247e1123182b84f53b0b8a5662bda206d47a4
Eric Minikel <ericminikel@gmail.com>
2014-10-24 17:26:58 -0400

    option to output individual genotypes or counts of each genotype

--- summarize_genotypes.py
+++ summarize_genotypes.py
@@ -16,6 +16,8 @@ parser.add_argument('--chrom', dest='chrom', action='store',
                     help='Chromosome of site of interest', type=str)
 parser.add_argument('--pos', dest='pos', action='store',
                     help='Position of site of interest', type=int)
+parser.add_argument('--summarize', dest='summarize', action='store_true',
+                    help='Print out genotype counts instead of individual genotypes')
 args = parser.parse_args()
 
 # read in the sample ids
@@ -28,7 +30,15 @@ vcf_header = get_vcf_header(args.vcf)
 
 genotypes = get_sample_genotypes(args.vcf,vcf_header,args.chrom,args.pos,sampleids)
 
-counts = dict(Counter(genotypes))
+if args.summarize:
+    # print out summary of results
+    counts = dict(Counter(genotypes.values()))
+    for key in counts.keys():
+        print "\t".join(map(str,[key,counts[key]]))
+else:
+    # print out results per individual
+    for key in genotypes.keys():
+        print "\t".join(map(str,[key,genotypes[key]]))
 
-for key in counts.keys():
-    print "\t".join(map(str,[key,counts[key]]))
+
+
